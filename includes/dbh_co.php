af1801ddcc346b44a16203c06b2e7e143bc109f5
Sakil GOULAMALY <sakil113@gmail.com>
2021-02-25 15:16:30 +0400

    update dbhco

--- includes/dbh_co.php
+++ includes/dbh_co.php
@@ -1,8 +1,8 @@
 <?php // connexion à la base de données
-    $db_username = 'root';
-    $db_password = '';
-    $db_name     = 'adrie';
-    $db_host     = 'localhost';
+    $db_username = 'sql11394899';
+    $db_password = 'g8H5UMKi9X';
+    $db_name     = 'sql11394899';
+    $db_host     = 'sql11.freesqldatabase.com';
     $db = mysqli_connect($db_host, $db_username, $db_password,$db_name)
            or die('could not connect to database');
 ?>
